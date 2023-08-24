d0040c7df65c7947aee47a48de596684a4f36c08
Sam1549 <Sam1549@yandex.ru>
2023-08-24 23:52:42 +0400

    removed (upgrade)

--- src/main/java/org/example/mapping/PostMapper.java
+++ src/main/java/org/example/mapping/PostMapper.java
@@ -2,11 +2,12 @@ package org.example.mapping;
 
 import org.example.model.Post;
 import org.example.model.PostDto;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-
+@Mapper
 public interface PostMapper {
     PostMapper INSTANCE = Mappers.getMapper(PostMapper.class);
 
